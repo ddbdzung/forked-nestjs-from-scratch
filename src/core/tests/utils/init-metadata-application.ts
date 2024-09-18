0b123e0de79455b165747a24730910c10165aba0
Dung Dang Duc Bao <dangdungth.wa@gmail.com>
2024-09-18 18:45:12 +0700

    test(core)(dungddb): add test for module class

---
+++ src/core/tests/utils/init-metadata-application.ts
@@ -0,0 +1,10 @@
+import { Type } from '@/core/interfaces/common.interface';
+import { ModuleFactory } from '@/core/module-factory';
+
+/**
+ * This function initializes the metadata application
+ * @param entry Entry class constructor (so called root module, main module)
+ */
+export function initMetadataApplication(entry: Type) {
+  return new ModuleFactory(entry);
+}
